6a1d6df2f3f01b63058a7d46ac53cf69ea08cba5
Kailash878 <panthakailash0806@gmail.com>
2026-07-28 17:49:14 +0545

    refactored locators and add new test cases

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,74 +1,118 @@
 
-const locators = {
-  usernameField:'[data-test="username"]',
-  passwordField:'[data-test="password"]',
-  loginButton:'[data-test="login-button"]',
-  errorMessageLocator:'[data-test="error"]',
-  crossMarkButton:'[data-icon="circle-xmark"]',
-  pageTitle:'[data-test="title"]',
-  addToCartButton:'[data-test="add-to-cart-sauce-labs-backpack"]',
- 
-};
-
-const testData={
-
-  valid:{
-    username:"standard_user",
-    password:"secret_sauce"
-  },
-
-  invalid:{
-    username:"jpt",
-    password:"123"
-  },
-  
- errorMessages:{
-    invalidCredentials:"Epic sadface: Username and password do not match any user in this service",
-    emptyUsernameMessage:"Epic sadface: Username is required",
-    emptyPasswordMessage:"Epic sadface: Password is required",
-    emptyUsernameAndPasswordMessage:"Epic sadface: Username is required"
-  }
-};
-
-
-
-
+import { locators } from "../practicelocators/locators.js";
+import { testData } from "../testdata/login.js";
 describe("Swag Labs Login", () => {
-  it("Login with valid credentials", () => {
+
+  beforeEach(() => {
     cy.visit("/");
-
-    cy.get(locators.usernameField).type(testData.valid.username);
-
-    cy.get(locators.passwordField).type(testData.valid.password);
+  })
+   
+  it("Login with valid credentials", () => {
     
-
-    cy.get(locators.loginButton).click();
-
+    cy.get(locators.loginLocators.usernameField).type(testData.valid.username);
+    cy.get(locators.loginLocators.passwordField).type(testData.valid.password);
+    cy.get(locators.loginLocators.loginButton).click();
     cy.url().should("include", "/inventory.html");
-
-    cy.get(locators.pageTitle).should("have.text", "Products");
-
-    cy.get(locators.addToCartButton).should("be.visible");
-
+    cy.get(locators.loginLocators.swagLabsLogo).should("be.visible");
 
 
   });
 
-  it.only("Login with empty credentials", () => {
-    cy.visit("/");
-    cy.get(locators.loginButton).click();
-    cy.get(locators.crossMarkButton).eq(0).should("be.visible");
-    cy.get(locators.crossMarkButton).eq(1).should("be.visible");
-
-    cy.get(locators.errorMessageLocator).should("have.text", testData.errorMessages.emptyUsernameAndPasswordMessage);
+  it("Login with empty credentials", () => {
+    
+    cy.get(locators.loginLocators.loginButton).click();
+    cy.get(locators.loginLocators.crossMarkButton).eq(0).should("be.visible");
+    cy.get(locators.loginLocators.crossMarkButton).eq(1).should("be.visible");
+    cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.emptyUsernameAndPasswordMessage);
 
   })
 
   it("Login with invalid credentials", () => {
-    cy.visit("/");
-    cy.get(locators.usernameField).type(testData.invalid.username);
-    cy.get(locators.passwordField).type(testData.invalid.password);
-    cy.get(locators.loginButton).click();
-    cy.get(locators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);    
+    
+    cy.get(locators.loginLocators.usernameField).clear().type(testData.invalid.username).should("have.value", testData.invalid.username);
+    cy.get(locators.loginLocators.passwordField).type(testData.invalid.password);
+    cy.get(locators.loginLocators.loginButton).click();
+    cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);    
   })
+
+  it("Login with valid username and empty password", () => {
+    
+    cy.get(locators.loginLocators.usernameField).clear().type(testData.valid.username).should("have.value", testData.valid.username);
+    cy.get(locators.loginLocators.loginButton).click();
+    cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.emptyPasswordMessage);    
+  })
+
+  it("Login with empty username and valid password", () => {
+    
+    cy.get(locators.loginLocators.passwordField).clear().type(testData.valid.password).should("have.value", testData.valid.password);
+    cy.get(locators.loginLocators.loginButton).click();
+    cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.emptyUsernameMessage);
+  })
+  
+  it("Login with invalid username and valid password", () => {
+    
+    cy.get(locators.loginLocators.usernameField).clear().type(testData.invalid.username).should("have.value", testData.invalid.username);
+    cy.get(locators.loginLocators.passwordField).clear().type(testData.valid.password).should("have.value", testData.valid.password);
+    cy.get(locators.loginLocators.loginButton).click();
+    cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);    
+  })
+
+  it("Login with valid username and invalid password", () => {
+    
+    cy.get(locators.loginLocators.usernameField).clear().type(testData.valid.username).should("have.value", testData.valid.username);
+    cy.get(locators.loginLocators.passwordField).clear().type(testData.invalid.password).should("have.value", testData.invalid.password);
+    cy.get(locators.loginLocators.loginButton).click();
+    cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);    
+  })
+
+it("Login with locked-out user", () => {
+  
+  cy.get(locators.loginLocators.usernameField).clear().type(testData.lockedOutUser.username).should("have.value", testData.lockedOutUser.username);
+  cy.get(locators.loginLocators.passwordField).clear().type(testData.lockedOutUser.password).should("have.value", testData.lockedOutUser.password);
+  cy.get(locators.loginLocators.loginButton).click();
+  cy.get(locators.loginLocators.errorMessageLocator).should("have.text", "Epic sadface: Sorry, this user has been locked out.");
+
+ })
+
+it("Login with uppercase username", () => {
+  
+  cy.get(locators.loginLocators.usernameField).clear().type(testData.valid.username.toUpperCase()).should("have.value", testData.valid.username.toUpperCase());
+  cy.get(locators.loginLocators.passwordField).clear().type(testData.valid.password).should("have.value", testData.valid.password);
+  cy.get(locators.loginLocators.loginButton).click();
+  cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);
+})
+
+it("Login with uppercase password", () => {
+  
+  cy.get(locators.loginLocators.usernameField).clear().type(testData.valid.username).should("have.value", testData.valid.username);
+  cy.get(locators.loginLocators.passwordField).clear().type(testData.valid.password.toUpperCase()).should("have.value", testData.valid.password.toUpperCase());
+  cy.get(locators.loginLocators.loginButton).click();
+  cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);
+
+})
+
+it("login with special characters in username and password", () => {
+  
+  cy.get(locators.loginLocators.usernameField).clear().type("!@#$%^&*()").should("have.value", "!@#$%^&*()");
+  cy.get(locators.loginLocators.passwordField).clear().type("!@#$%^&*()").should("have.value", "!@#$%^&*()");
+  cy.get(locators.loginLocators.loginButton).click();
+  cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);
+})
+
+it("login with whitespace in username and password", () => {
+  
+  cy.get(locators.loginLocators.usernameField).clear().type("   ").should("have.value", "   ");
+  cy.get(locators.loginLocators.passwordField).clear().type("   ").should("have.value", "   ");
+  cy.get(locators.loginLocators.loginButton).click();
+  cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);
+
+})
+
+it("login with SQL injection in username and password", () => {
+  
+  cy.get(locators.loginLocators.usernameField).clear().type("' OR '1'='1").should("have.value", "' OR '1'='1");
+  cy.get(locators.loginLocators.passwordField).clear().type("' OR '1'='1").should("have.value", "' OR '1'='1");
+  cy.get(locators.loginLocators.loginButton).click();
+  cy.get(locators.loginLocators.errorMessageLocator).should("have.text", testData.errorMessages.invalidCredentials);
+})
 });
